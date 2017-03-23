4c4795c75451a9522ae043b0b93d5ae648fe5739
francisrodsonclavel <francisrodsonclavel@gmail.com>
2017-03-23 17:36:09 -0400

    Create ftexpandable_raw.js

---
+++ ftexpandable_raw.js
@@ -0,0 +1,27 @@
+<noscript>
+<a href="http://servedby.flashtalking.com/click/1/65796;2421656;0;209;0/?ft_width=320&ft_height=50&url=14158209" target="_blank">
+<img border="0" src="http://servedby.flashtalking.com/imp/1/65796;2421656;205;gif;TubeMogul;120x600ExpSkyStrogbowcloudyapple/?"></a>
+</noscript>
+<script language="Javascript1.1" type="text/javascript">
+var ftClick = "";
+var ftExpTrack_2421656 = "";
+var ftX = "";
+var ftY = "";
+var ftZ = "";
+var ftOBA = 1;
+var ftContent = "";
+var ftCustom = "";
+var ftSection = " ${TM_SITE_ID_NUM}";
+var ftID = function(){for(var e=["${USER_ID}"],a=e.length,r="";a--;)if(e[a]&&!RegExp("[^a-z0-9q-]".replace(/q/g,decodeURIComponent("%5"+"C")),"i").test(e[a])){r=e[a];break}return r}();;
+var ft320x50_OOBclickTrack = "";
+var ftRandom = Math.random()*1000000;
+var ftBuildTag1 = "<scr";
+var ftBuildTag2 = "</";
+var ftClick_2421656 = ftClick;
+if(typeof(ft_referrer)=="undefined"){var ft_referrer=(function(){var r="";if(window==top){r=window.location.href;}else{try{r=window.parent.location.href;}catch(e){}r=(r)?r:document.referrer;}while(encodeURIComponent(r).length>1000){r=r.substring(0,r.length-1);}return r;}());}
+var ftDomain = (window==top)?"":(function(){var d=document.referrer,h=(d)?d.match("(?::q/q/)+([qw-]+(q.[qw-]+)+)(q/)?".replace(/q/g,decodeURIComponent("%"+"5C")))[1]:"";return (h&&h!=location.host)?"&ft_ifb=1&ft_domain="+encodeURIComponent(h):"";}());
+var ftTag = ftBuildTag1 + 'ipt language="javascript1.1" type="text/javascript" ';
+ftTag += 'src="http://servedby.flashtalking.com/imp/1/65796;2421656;201;js;TubeMogul;120x600ExpSkyStrogbowcloudyapple/?ftx='+ftX+'&fty='+ftY+'&ftadz='+ftZ+'&ftscw='+ftContent+'&ft_custom='+ftCustom+'&ft_section='+ftSection+'&ft_id='+ftID+'&ftOBA='+ftOBA+ftDomain+'&ft_agentEnv='+(window.mraid%7C%7Cwindow.ormma?'1':'0')+'&ft_referrer='+encodeURIComponent(ft_referrer)+'&cachebuster='+ftRandom+'" id="ftscript_320x50" name="ftscript_320x50"';
+ftTag += '>' + ftBuildTag2 + 'script>';
+document.write(ftTag);
+</script>
