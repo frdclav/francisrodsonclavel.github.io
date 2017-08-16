183240241d44a5829ce91074e8f53adce34f2d07
francisrodsonclavel <francisrodsonclavel@gmail.com>
2017-08-16 13:37:01 -0700

    Create display.js

---
+++ display.js
@@ -0,0 +1,110 @@
+try { // standard_display: 
+  try {
+    var ast_version = "20170613";
+    if (typeof(TM_array) == "undefined") {
+      var TM_array = []
+    }
+    var uIdx = Math.random() + Date.now().valueOf();
+    TM_array[uIdx] = {
+      Args: {}
+    };
+    TM_array[uIdx].Args.data = [{
+      "preview": false,
+      "session": {
+        "host": "as.tubemogul.com"
+      },
+      "adEngClickThruQP": "&cak=QbpRQp0O6T0ZoW7tEda8&pai=1902423795&eew=14&eec=3",
+      "closeButtonDelay": 0,
+      "tmCampaignIdNum": 647608,
+      "iasMon": "T",
+      "adParts": [{
+        "sequence": 0,
+        "assets": [{
+          "mimeType": "text/html",
+          "type": "display_code",
+          "id": "10709841",
+          "detail": "<script type=\"text/javascript\" src=\"//ads.everesttech.net/ads/mts/9653/5132?CLK_TR=&CMP_ID=647608&PLCMT_ID=1552651&AD_ID=4116756492&FEED_ID=89765&cachebuster=1502915805207&TC_1=4116756492&TC_2=1552651&TC_3=swZPzSq9uRmsiDELNCQT\"></script><noscript><iframe width=\"300\" height=\"250\" scrolling=\"no\" frameborder=\"0\" allowtransparency=\"true\" hspace=\"0\" vspace=\"0\" marginwidth=\"0\" marginheight=\"0\" style=\"margin:0px;\" src=\"//ads.everesttech.net/ads/mti/9653/5132?CLK_TR=&CMP_ID=647608&PLCMT_ID=1552651&AD_ID=4116756492&FEED_ID=89765&cachebuster=1502915805207&TC_1=4116756492&TC_2=1552651&TC_3=swZPzSq9uRmsiDELNCQT&njs=n\"></iframe></noscript>",
+          "resourceType": "static"
+        }],
+        "width": "100%",
+        "action": "null",
+        "type": "display",
+        "height": "100%"
+      }, {
+        "sequence": 99,
+        "assets": [],
+        "width": "100%",
+        "action": "null",
+        "type": "fallback",
+        "height": "100%"
+      }],
+      "iasAnId": 5838,
+      "bt": true,
+      "tmFeedIdNum": 89765,
+      "feedId": "VLk2hfJ0gD0TStCupYH4",
+      "adClass": "standard_display",
+      "paId": "XhSuDZ4WW2Zh9HDMWCbl",
+      "adEngImpQP": "&cak=QbpRQp0O6T0ZoW7tEda8&pai=1902423795&eew=37&eec=1",
+      "wmode": "window",
+      "moatEnabled": "F",
+      "height": 250,
+      "tmAdIdNum": 4116756492,
+      "adPluginURL": "/flash/tunit-v1.55.1.swf",
+      "viewabilityStandard": "MRC",
+      "statsBaseURL": "stats-tm.everesttech.net/stats/1",
+      "displayType": "inbanner_3rd_party",
+      "pixels": [{
+        "id": "TM_EVEREST_G_2",
+        "trigger": "TM_EVEREST_G_2",
+        "detail": "http://rtd.tubemogul.com/migrate_et1?did=&everest_g_v2=WZSs3QAAAeGdkX07",
+        "type": "Flash"
+      }, {
+        "id": "TM_AUDIT_COMP",
+        "trigger": "TM_AUDIT_COMP",
+        "detail": "http://ad-audit.tubemogul.com/audit/e.png?event=comp&mp=VLk2hfJ0gD0TStCupYH4&cp=JBD7gQxXlO3yFWgPNhNX&pa=XhSuDZ4WW2Zh9HDMWCbl&pv=cpPIO7hWJpjKP9E5UG5L&rand=297432546",
+        "type": "Flash"
+      }, {
+        "id": "TM_AUDIT_VIEW",
+        "trigger": "TM_AUDIT_VIEW",
+        "detail": "http://ad-audit.tubemogul.com/audit/e.png?event=view&mp=VLk2hfJ0gD0TStCupYH4&cp=JBD7gQxXlO3yFWgPNhNX&pa=XhSuDZ4WW2Zh9HDMWCbl&pv=cpPIO7hWJpjKP9E5UG5L&rand=1591713233",
+        "type": "Flash"
+      }, {
+        "id": "TM_AUDIT_CODE",
+        "trigger": "TM_AUDIT_CODE",
+        "detail": "http://ad-audit.tubemogul.com/audit/e.png?event=code&mp=VLk2hfJ0gD0TStCupYH4&cp=JBD7gQxXlO3yFWgPNhNX&pa=XhSuDZ4WW2Zh9HDMWCbl&pv=cpPIO7hWJpjKP9E5UG5L&rand=1286973588",
+        "type": "Flash"
+      }, {
+        "id": "TM_AUDIT_TAG",
+        "trigger": "TM_AUDIT_TAG",
+        "detail": "http://ad-audit.tubemogul.com/audit/e.png?event=tag&mp=VLk2hfJ0gD0TStCupYH4&cp=JBD7gQxXlO3yFWgPNhNX&pa=XhSuDZ4WW2Zh9HDMWCbl&pv=cpPIO7hWJpjKP9E5UG5L&rand=1054216193",
+        "type": "Flash"
+      }, {
+        "id": "TM_AUDIT_REND",
+        "trigger": "TM_AUDIT_REND",
+        "detail": "http://ad-audit.tubemogul.com/audit/e.png?event=rend&mp=VLk2hfJ0gD0TStCupYH4&cp=JBD7gQxXlO3yFWgPNhNX&pa=XhSuDZ4WW2Zh9HDMWCbl&pv=cpPIO7hWJpjKP9E5UG5L&rand=-156514189",
+        "type": "Flash"
+      }],
+      "adId": "XhSuDZ4WW2Zh9HDMWCbl",
+      "width": 300,
+      "assetBaseURL": "//playtime.tubemogul.com",
+      "tmSiteIdNum": "",
+      "adEngViewQP": "&cak=QbpRQp0O6T0ZoW7tEda8&pai=1902423795&eew=37&eec=2",
+      "tmPlacementIdNum": 1552651
+    }];
+    var tm_auction_id = TM_array[uIdx].Args.data[0].session.sessId;
+    TM_array[tm_auction_id] = TM_array[uIdx];
+    delete TM_array[uIdx];
+    TM_array[tm_auction_id].Feed = TM_array[tm_auction_id].Args.data[0];
+    var script = document.createElement("script");
+    script.src = "https://playtime.tubemogul.com/prod/display_transitional/release/std_disp_plugin-b220e7.js";
+    document.getElementsByTagName("head")[0].appendChild(script)
+  } catch (e) {
+    img = new Image();
+    img.src = "https://playtime.tubemogul.com/adtags/no_javascript.jpg?reason=error_caught:" + encodeURIComponent(e.message);
+    document.body.appendChild(img)
+  };
+} catch (e) {
+  img = new Image();
+  img.src = 'https//playtime.tubemogul.com/adtags/no_javascript.jpg?reason=error_caught:' + encodeURIComponent(e.message);
+  document.body.appendChild(img);
+}
