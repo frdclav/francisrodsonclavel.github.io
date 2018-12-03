8ae419c720768080715e0f1dd143893806aef60f
francisrodsonclavel <francisrodsonclavel@gmail.com>
2018-12-03 11:44:23 -0800

    Create 20181203.js

---
+++ 20181203.js
@@ -0,0 +1,3 @@
+
+<script src="mraid.js"></script>
+<script type="text/javascript" src="http://displayf-tm.everesttech.net/feed/placement.js/?id=Xf6e2yqPYRavegdigOp6&q=H4sIAAAAAAAAAOMUOqbc80W0JVGUeXVXuFQtl1ZJZUGqXnp-fnpOamJBZrFecn6uflJmSkpqkZ5jSnBqUVlqUVBqYWlqcYmQv8RlFiWRiDSzVKPKwoDIoMSy1PSUzHT_AjONd-8qjERCDUL8kzIKo0pK8oJzUy18DbMCiywefFy8mt9JJCfUrNgto9jZJTw7qNiyzKvUyd9bcWX0S5vs49kbAHeu-tqWAAAA"></script>
